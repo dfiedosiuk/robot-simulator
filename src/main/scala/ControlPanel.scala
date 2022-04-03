3166f3119104fc35eec856efb6483cc34bba10ae
Daniel <d.fiedosiuk@adform.com>
2022-04-03 20:43:01 +0200

    Test added for invalid command

--- src/main/scala/ControlPanel.scala
+++ src/main/scala/ControlPanel.scala
@@ -6,10 +6,18 @@ object ControlPanel extends App {
         case 'A' => rob.moveForward()
         case 'L' => rob.turnLeft()
         case 'R' => rob.turnRight()
+        case x => println(s"${x} is invalid input!"); rob
       }
     }
-    println(s"Robot is now in position ${nextRobot.x},${nextRobot.y} and facing ${nextRobot.angle}")
+    println(
+      s"Robot is now in position {${nextRobot.x},${nextRobot.y}} " +
+      s"and facing ${
+      nextRobot.angle match {
+        case -90 => "west"
+        case 0 => "north"
+        case 90 => "east"
+        case 180 => "south"
+      }}")
     nextRobot
   }
-
 }
--- src/test/scala/RobotSpec.scala
+++ src/test/scala/RobotSpec.scala
@@ -1,7 +1,7 @@
 import org.scalatest._
 import flatspec._
 import matchers._
-import Robot._
+
 import ControlPanel._
 
 
@@ -13,4 +13,7 @@ class ControlPanelSpec extends AnyFlatSpec with should.Matchers {
     executeCommands(Robot(7, 3, 0), "RAALAL") shouldBe Robot(9, 4, -90)
   }
 
+  it should "return new object Robot with parameters corresponding to the commands skipping non-existing commands" in {
+    executeCommands(Robot(7, 3, 0), "RAALALALWA") shouldBe Robot(8, 3, 180)
+  }
 }
